40d11cdf145a897b7d8fd64e7254bf43f634180c
Jelle Aalbers <jelleaalbers@gmail.com>
2024-01-05 15:29:24 +0100

    Remove old warning

--- wimprates/__init__.py
+++ wimprates/__init__.py
@@ -1,12 +1,5 @@
 __version__ = '0.5.0'
 
-from packaging import version
-if version.parse(__version__) < version.parse('0.6.0'):
-    import warnings
-    warnings.warn(
-        'Default WIMP parameters are changed in accordance with '
-        'https://arxiv.org/abs/2105.00599 (github.com/JelleAalbers/wimprates/pull/14)')
-
 from .utils import *
 from .halo import *
 from .srdm import *
@@ -15,4 +8,3 @@ from .bremsstrahlung import *
 from .migdal import *
 from .electron import *
 from .summary import *
-
