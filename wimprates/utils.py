5dc19acb32cfacf7a96e197b195aa2a6973cfca2
Lorenzo Principe <28869147+lorenzomag@users.noreply.github.com>
2024-08-06 12:12:27 +0200

    Add utility function to preformat data for Cox's interpolators

--- wimprates/utils.py
+++ wimprates/utils.py
@@ -4,7 +4,7 @@ import pickle
 
 from boltons.funcutils import wraps
 import numpy as np
-from tqdm import tqdm
+from tqdm.autonotebook import tqdm
 
 
 def exporter():
@@ -60,3 +60,48 @@ def vectorize_first(f):
                  for x in itr(xs)])
         return f(xs, *args, **kwargs)
     return newf
+
+
+@export
+def pairwise_log_transform(a, b):
+    """
+    Preprocesses two input arrays by reshaping, concatenating, and applying a logarithmic transformation.
+
+    This function takes two input arrays (or single values), reshapes them into column vectors,
+    concatenates them horizontally to form a 2D array, and then applies the natural logarithm
+    to each element of the resulting array. It ensures compatibility with the downstream function
+    that expects a 2D array with two columns, regardless of whether the input consists of single
+    values or multiple pairs of values.
+
+    Parameters
+    ----------
+    a : array-like or float
+        The first input array or single float value.
+    b : array-like or float
+        The second input array or single float value.
+
+    Returns
+    -------
+    numpy.ndarray
+        A 2D array with shape (n, 2), where n is the number of elements in the input arrays.
+        Each row contains the natural logarithm of the corresponding elements from the input arrays.
+
+    Examples
+    --------
+    >>> pairwise_log_transform([4.5, 2.7], [400, 900])
+    array([[ 1.5040774 ,  5.99146455],
+           [ 0.99325177,  6.80239476]])
+
+    >>> pairwise_log_transform(4.5, 400)
+    array([[1.5040774 , 5.99146455]])
+
+    Notes
+    -----
+    - If `a` and `b` are not arrays, they will be converted to arrays.
+    - If `a` and `b` are single values, the output will be a 2D array with a single row.
+    - The function applies `np.log` to each element in the concatenated array.
+    """
+    a = np.atleast_1d(a).reshape(-1, 1)
+    b = np.atleast_1d(b).reshape(-1, 1)
+    arr = np.concatenate((a, b), axis=1)
+    return np.log(arr)
